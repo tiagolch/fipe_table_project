0637750d96288e96e1298e1f88199fecda0908ef
Marcelo <marceloromeugoncalves@gmail.com>
2021-12-02 13:25:36 -0300

    Evento de mudança do tipo de veículo.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function () {
-    $('#tipo_veiculo').on('change', function () {
-        alert(this.value);
+    jQuery('#tipo_veiculo').on('change', function () {
+        let typeOfVehicle = this.value;
+
+        if (typeOfVehicle === ""){
+            jQuery('#marca').attr("disabled", true);
+        }
+        else {
+            jQuery("#marca").attr("disabled", false);
+        }
     });
 });
