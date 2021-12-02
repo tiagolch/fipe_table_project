5f9f488cd60b7fda3ba2c020da280857e9d1eb01
Marcelo <marceloromeugoncalves@gmail.com>
2021-12-02 15:56:26 -0300

    Preenchendo o select da marca dinamicamente.

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,34 @@
+function clearOptions(idSelect) {
+    //Removendo as opções.
+    jQuery(`${idSelect} option`).remove();
+    jQuery(idSelect).selectpicker('refresh');
+}
+
 jQuery(document).ready(function () {
     jQuery('#tipo_veiculo').on('change', function () {
         let typeOfVehicle = this.value;
 
-        if (typeOfVehicle === ""){
+        if (typeOfVehicle === "") {
+            clearOptions('#marca');
             jQuery('#marca').prop('disabled', true);
             jQuery('#marca').selectpicker('refresh');
         }
         else {
             jQuery('#marca').prop('disabled', false);
             jQuery('#marca').selectpicker('refresh');
+
+            let url = `/marks_json/?tipo_veiculo=${typeOfVehicle}`;
+
+            clearOptions('#marca');
+
+            jQuery.get(url, function (data) {
+                for (let i = 0; i < data.length; i++) {
+                    let item = data[i];
+                    jQuery('#marca').append(new Option(item.nome, item.codigo));
+                }
+
+                jQuery('#marca').selectpicker('refresh');
+            });
         }
     });
 });
